e6dd07a62fc5dc2b444f753805d96da0cdf3a537
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-10-17 09:57:09 +0600

    fix dockerfile php 7.4 bitrix

--- internal/config/root.go
+++ internal/config/root.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	ScriptName            string = "docky"
-	ScriptVersion         string = "2.2.19"
+	ScriptVersion         string = "2.2.20"
 	SiteDirName           string = "site"
 	DockerFilesDirName    string = "_docker"
 	ConfFilesDirName      string = "_conf"
