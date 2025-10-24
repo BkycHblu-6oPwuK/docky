b10ce3d476af103b674fe984ee11e1af6c760f9c
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-10-24 09:48:35 +0600

    fix node dockerfile + php 7.1 for bitrix

--- internal/composefiletools/root.go
+++ internal/composefiletools/root.go
@@ -50,7 +50,7 @@ func GetAvailableVersions(service string, yamlConfig *config.YamlConfig) []strin
 		case framework.Laravel, framework.Symfony, framework.Vanilla:
 			return []string{"8.2", "8.3", "8.4"}
 		default:
-			return []string{"5.6", "7.4", "8.1", "8.2", "8.3", "8.4"}
+			return []string{"5.6", "7.1", "7.4", "8.1", "8.2", "8.3", "8.4"}
 		}
 	case Mysql:
 		switch yamlConfig.FrameworkName {
--- internal/config/root.go
+++ internal/config/root.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	ScriptName            string = "docky"
-	ScriptVersion         string = "2.2.20"
+	ScriptVersion         string = "2.2.21"
 	SiteDirName           string = "site"
 	DockerFilesDirName    string = "_docker"
 	ConfFilesDirName      string = "_conf"
