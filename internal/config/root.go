adc485431541df2e360e3a131ffe6c0b41497241
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-11-07 10:28:07 +0600

    fix php  7.4 buitrix

--- internal/config/root.go
+++ internal/config/root.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	ScriptName            string = "docky"
-	ScriptVersion         string = "2.2.28"
+	ScriptVersion         string = "2.2.29"
 	SiteDirName           string = "site"
 	DockerFilesDirName    string = "_docker"
 	ConfFilesDirName      string = "_conf"
