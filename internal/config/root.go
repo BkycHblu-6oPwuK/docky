c87cc33bc15544a632ab3514ff7351d3aa4fdc4b
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-11-12 14:02:24 +0600

    fix php 5.6 ext GD configure

--- internal/config/root.go
+++ internal/config/root.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	ScriptName            string = "docky"
-	ScriptVersion         string = "2.2.29"
+	ScriptVersion         string = "2.2.30"
 	SiteDirName           string = "site"
 	DockerFilesDirName    string = "_docker"
 	ConfFilesDirName      string = "_conf"
